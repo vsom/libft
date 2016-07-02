6131b0a7ae06dc19d5ca79d620852e7625f8dc61
Vann Som <vsodanny@gmail.com>
2016-07-02 16:15:54 +0200

    first step

---
+++ ft_putnbr.c
@@ -0,0 +1,17 @@
+void	ft_putnbr(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+	{
+		ft_putchar(n + '0');
+	}
+}
---
+++ ft_putstr.c
@@ -0,0 +1,11 @@
+void	ft_putstr(char const *s)
+{
+	int i;
+
+	int = 0;
+	while (s[i] != '\0')
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+}
